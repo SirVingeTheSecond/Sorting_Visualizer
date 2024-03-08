087cb3420054fedb0fe19314117050772df37025
MV <mving23@student.sdu.dk>
2024-03-08 16:15:29 +0100

    Re-added various sorting algorithms

--- SortingVisualizer/src/main/java/org/example/sortingvisualizer/Interface/ISortingAlgorithmSupplier.java
+++ SortingVisualizer/src/main/java/org/example/sortingvisualizer/Interface/ISortingAlgorithmSupplier.java
@@ -2,7 +2,7 @@ package org.example.sortingvisualizer.Interface;
 
 import org.example.sortingvisualizer.Utility.Sorter;
 
-public interface SortingAlgorithmSupplier {
+public interface ISortingAlgorithmSupplier {
     ISortingAlgorithm get(ISortUpdateListener listener, Sorter sorter);
 }
 
--- SortingVisualizer/src/main/java/org/example/sortingvisualizer/SortingAlgorithmFactory.java
+++ SortingVisualizer/src/main/java/org/example/sortingvisualizer/SortingAlgorithmFactory.java
@@ -2,8 +2,8 @@ package org.example.sortingvisualizer;
 
 import org.example.sortingvisualizer.Interface.ISortUpdateListener;
 import org.example.sortingvisualizer.Interface.ISortingAlgorithm;
-import org.example.sortingvisualizer.Interface.SortingAlgorithmSupplier;
-import org.example.sortingvisualizer.SortingAlgorithms.BubbleSort;
+import org.example.sortingvisualizer.Interface.ISortingAlgorithmSupplier;
+import org.example.sortingvisualizer.SortingAlgorithms.*;
 import org.example.sortingvisualizer.Utility.Sorter;
 
 import java.util.ArrayList;
@@ -12,16 +12,20 @@ import java.util.List;
 import java.util.Map;
 
 public class SortingAlgorithmFactory {
-    private static Map<String, SortingAlgorithmSupplier> algorithms = new HashMap<>();
+    private static Map<String, ISortingAlgorithmSupplier> algorithms = new HashMap<>();
 
-    // When the class is loaded into memory
+    // Executed when the class is loaded into memory
     static {
         algorithms.put("Bubble Sort", (listener, sorter) -> new BubbleSort(listener, sorter));
-        // Add other algorithms similarly
+        algorithms.put("Insertion Sort", (listener, sorter) -> new InsertionSort(listener, sorter));
+        algorithms.put("Merge Sort", (listener, sorter) -> new MergeSort(listener, sorter));
+        algorithms.put("Qucik Sort", (listener, sorter) -> new QuickSort(listener, sorter));
+        algorithms.put("Radix Sort", (listener, sorter) -> new RadixSort(listener, sorter));
+        // New algorithms goes here
     }
 
     public static ISortingAlgorithm createAlgorithm(String algorithmName, ISortUpdateListener listener, Sorter sorter) {
-        SortingAlgorithmSupplier algorithmSupplier = algorithms.get(algorithmName);
+        ISortingAlgorithmSupplier algorithmSupplier = algorithms.get(algorithmName);
         if (algorithmSupplier == null) {
             throw new IllegalArgumentException("Unknown algorithm: " + algorithmName);
         }
